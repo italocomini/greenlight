2e4be93c1e882e3703bd42758edf102e08f0f9e8
Italo Comini <italocomini@gmail.com>
2021-05-15 16:20:22 -0300

    cp-3.6 - sending error messages

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,14 +7,15 @@ import (
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 
 	data := envelope{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
